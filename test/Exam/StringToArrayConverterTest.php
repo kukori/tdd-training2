bd61a9e2684bf7d9f869f73e37d9d95dc833c230
kukori <attila.varjas@gmail.com>
2015-03-12 16:49:12 +0100

    1.2

--- test/Exam/StringToArrayConverterTest.php
+++ test/Exam/StringToArrayConverterTest.php
@@ -3,11 +3,25 @@
 use Tdd\Exam\StringToArrayConverter;
 class StringToArrayConverterTest extends \PHPUnit_Framework_TestCase
 {
-	public function testConvert()
+	/**
+	 *
+	 * @param string $string
+	 * @param array $resultArray
+	 *
+	 * @dataProvider convertTestDataProvider
+	 */
+	public function testConvert($string, $resultArray)
 	{
-		$string = "a,b,c";
-		$resultArray = array ('a', 'b', 'c');
 		$converter = new StringToArrayConverter();
 		$this->assertEquals($resultArray, $converter->convert($string));
+	}
+
+	public function convertTestDataProvider()
+	{
+		return array(
+			array('a,b,c', array ('a', 'b', 'c')),
+			array('100,982,444,990,1', array ('100', '982', '444', '990', '1')),
+			array('Mark,Anthony,marka@lib.de', array ('Mark', 'Anthony', 'marka@lib.de')),
+		);
 	}
 }
